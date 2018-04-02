8ca3fc1eca75a913c8044b4f6c0a982292c07f56
pornchai <chaweewat.p@gmail.com>
2018-04-03 00:28:49 +0700

    add firebase script

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 //current energy consumption update picture
 function update_data(id, number) {
     $("#"+String(id)).text(String(number));
+    console.log("update data!!!!");
+
 }
 $( document ).ready(function() {
     console.log("starting document!!!!");
@@ -8,20 +10,20 @@ $( document ).ready(function() {
     // Initialize Firebase
     console.log("Initialize Firebase");
     var config = {
-      apiKey: "AIzaSyCZpAgvsJjoOVdNyDAlFDgE1n3HwSlb5Sw",
-      authDomain: "thesis-650a3.firebaseapp.com",
-      databaseURL: "https://thesis-650a3.firebaseio.com",
-      projectId: "thesis-650a3",
-      storageBucket: "thesis-650a3.appspot.com",
-      messagingSenderId: "210862510550"
-    };
-    firebase.initializeApp(config);
+    apiKey: "AIzaSyCZpAgvsJjoOVdNyDAlFDgE1n3HwSlb5Sw",
+    authDomain: "thesis-650a3.firebaseapp.com",
+    databaseURL: "https://thesis-650a3.firebaseio.com",
+    projectId: "thesis-650a3",
+    storageBucket: "thesis-650a3.appspot.com",
+    messagingSenderId: "210862510550"
+  };
+  firebase.initializeApp(config);
 
     var ref = firebase.database().ref();
 
-    var member_profitRef = firebase.database().ref("PJM_5_bus_PIPS");
+    var numRef = firebase.database().ref("overall");
 
-    member_profitRef.on("child_changed", function(data) {
+    numRef.on("child_changed", function(data) {
         console.log(data.key);
         console.log(data.val());
         if(data.key == "1PV221445K1200100") {
@@ -30,5 +32,7 @@ $( document ).ready(function() {
            update_data(data.key, parseInt(data.val()))
         }
     });
+
+
 
 });
